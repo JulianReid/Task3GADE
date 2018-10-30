a1ff436686b5c324156b5bfa73faee74e0352a78
JulianReid <43538022+JulianReid@users.noreply.github.com>
2018-10-30 12:02:16 +0200

    Add files via upload

--- NEWTASK 3/NEWTASK 3/Form1.Designer.cs
+++ NEWTASK 3/NEWTASK 3/Form1.Designer.cs
@@ -28,21 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblMap = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.BTNEXIT = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblMap
+            // 
+            this.lblMap.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblMap.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMap.Location = new System.Drawing.Point(26, 45);
+            this.lblMap.Name = "lblMap";
+            this.lblMap.Size = new System.Drawing.Size(579, 621);
+            this.lblMap.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(642, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(251, 70);
+            this.label2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(647, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(691, 253);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(151, 66);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(691, 347);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(151, 66);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "PAUSE";
+            this.btnPause.UseVisualStyleBackColor = true;
+            // 
+            // BTNEXIT
+            // 
+            this.BTNEXIT.Location = new System.Drawing.Point(691, 452);
+            this.BTNEXIT.Name = "BTNEXIT";
+            this.BTNEXIT.Size = new System.Drawing.Size(151, 66);
+            this.BTNEXIT.TabIndex = 5;
+            this.BTNEXIT.Text = "EXIT";
+            this.BTNEXIT.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(931, 703);
+            this.Controls.Add(this.BTNEXIT);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMap);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMap;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button BTNEXIT;
     }
 }
 
